7edcc5aa3deed4c065d53545347a2b047da17f59
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-08 09:26:10 +0600

    Add return type test

---
+++ ProjectToTest.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ProjectToTest.UnitTests
+{
+    [TestFixture]
+    internal class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+            //Exactly NotFound Object
+            Assert.That(result, Is.TypeOf<NotFound>());
+            //NotFound Object or derivatives/children
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk() 
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
