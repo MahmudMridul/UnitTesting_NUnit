e7cf03f2c6945616c4b4e60f5670b3420889290d
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-08 09:24:41 +0600

    Add void method test

---
+++ ProjectToTest.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ProjectToTest.UnitTests
+{
+    [TestFixture]
+    internal class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            ErrorLogger logger = new ErrorLogger();
+            logger.Log("Some error");
+            Assert.That(logger.LastError, Is.EqualTo("Some error"));
+        }
+    }
+}
