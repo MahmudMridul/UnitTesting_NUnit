209517e7f2337a6ef10e8dd29ee47d80daa9b3e0
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-08 09:24:24 +0600

    Add string tests

---
+++ ProjectToTest.UnitTests/StringFunctionsTests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ProjectToTest.UnitTests
+{
+    [TestFixture]
+    internal class StringFunctionsTests
+    {
+        [Test]
+        public void SurroundUnderScore_WhenCalled_ReturnUnderScoreSurroundedString()
+        {
+            StringFunctions func = new StringFunctions();
+            string result = func.SurroundUnderScore("A quick brown fox");
+            //Specific
+            Assert.That(result, Is.EqualTo("__A quick brown fox__"));
+
+            //General
+            //Assert.That(result, Does.StartWith("__"));
+            //Assert.That(result, Does.EndWith("__"));
+            //Assert.That(result, Does.Contain("A quick brown fox"));
+        }
+    }
+}
---
+++ ProjectToTest/StringFunctions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ProjectToTest
+{
+    public class StringFunctions
+    {
+        public string SurroundUnderScore(string content)
+        {
+            return $"__{content}__";
+        }
+    }
+}
