bad338dfa7e3d11f516b3dc9c675c493138a7ff3
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-09 08:17:51 +0600

    Add tests for exception and event

--- ProjectToTest.UnitTests/ErrorLoggerTests.cs
+++ ProjectToTest.UnitTests/ErrorLoggerTests.cs
@@ -4,12 +4,38 @@ namespace ProjectToTest.UnitTests
     [TestFixture]
     internal class ErrorLoggerTests
     {
+        private ErrorLogger logger;
+        [SetUp]
+        public void SetUp()
+        {
+            logger = new ErrorLogger();
+        }
+
         [Test]
         public void Log_WhenCalled_SetTheLastErrorProperty()
         {
-            ErrorLogger logger = new ErrorLogger();
             logger.Log("Some error");
             Assert.That(logger.LastError, Is.EqualTo("Some error"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            Guid id = Guid.Empty;
+            //when Log function is called it will raise an event as 
+            //a result id will be set to some guid
+            logger.ErrorLogged += (sender, args) => { id = args; };
+            logger.Log("some log");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
