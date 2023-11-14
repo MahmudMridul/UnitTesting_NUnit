ad4945066251641c1479aadbd4c57a1c4d53ec5e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-14 08:33:15 +0600

    Test commit

--- ProjectToTest/FizzBuzz.cs
+++ ProjectToTest/FizzBuzz.cs
@@ -11,5 +11,13 @@ namespace ProjectToTest
             if (number % 5 == 0) { return "Buzz"; }
             return number.ToString();
         }
+
+        public string GetOutput(int num1, int num2)
+        {
+            if(num1 % 3 == 0 && num2 % 5 == 0) { return "FizzBuzz"; }
+            if(num1 % 3 == 0) { return "Fizz"; }
+            if(num2 % 5 == 0) { return "Buzz"; }
+            return num1.ToString() + num2.ToString();
+        }
     }
 }
